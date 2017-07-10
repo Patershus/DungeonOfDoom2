a809ee128667ba2f9467dd3e5c9ebd07e25840bc
Petter Olgemar <petterolgemar_92@hotmail.com>
2017-07-10 10:25:25 +0200

    Lagt till lite kommentarer

--- Spel/DoD/Wizard.cs
+++ Spel/DoD/Wizard.cs
@@ -9,12 +9,12 @@ namespace DoD
 {
     public class Wizard : Monster
     {
+        //array av spell strings, lambda, skulle kunna vara referens till namngivna metoder
         Func<Character, Character, string>[] spells =
             {
             (spellCaster, target) => {target.Health -= 10; return $"{spellCaster}s spell damaged {target} for 10"; },
             (spellCaster, target) => {spellCaster.Health += 10; return $"{spellCaster} healed for 10"; },
             (spellCaster, target) => {target.Health -= 5;spellCaster.Health+=5; return $"{spellCaster} drain 5 health from {target}"; },
-
         };
 
         public Wizard(int x, int y) : base(40, 0, x, y)
@@ -24,16 +24,12 @@ namespace DoD
 
         public override string Attack(Character opponent)
         {
-
-            //Cast spell 
+            //Väljer en random spell
             int spellNumber = RandomUtils.RandomNumber(0, spells.Length-1);
             Func<Character, Character, string> spell = spells[spellNumber];
 
-            //spell.Invoke(this, opponent);
-            //fireBall.Invoke(this, opponent);
-            
+            //Använder spell
              return spell.Invoke(this, opponent);
-
         }
 
         public override string ToString()
